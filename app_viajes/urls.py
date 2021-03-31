81e423b779d7edb15def9d8890a812e6d93421ca
devel@mauriciocap.com <devel@mauriciocap.com>
2021-03-31 20:45:02 -0300

    FIX: la api se muestra en api/

--- app_viajes/urls.py
+++ app_viajes/urls.py
@@ -10,6 +10,6 @@ router.register(r'lugar', views.LugarViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
