d26167c3856cc430859614a64134d7cd6e8c3d82
devel@mauriciocap.com <devel@mauriciocap.com>
2021-03-31 21:18:27 -0300

    OK: agruegue app react como submodule git

--- app_viajes/urls.py
+++ app_viajes/urls.py
@@ -9,7 +9,12 @@ router.register(r'lugar', views.LugarViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+	[
+			path('api/', include(router.urls)),
+			path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+	] + 
+	static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + #A: imagenes subidas para lugar
+	static('app/', document_root=settings.BASE_DIR / 'app_viajes_browser/static' ) #A: la app react
+)
+
